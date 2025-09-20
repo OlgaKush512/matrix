99516b6db88f93ecc529f27f6bccd369d720e2fc
Your Name <you@example.com>
2025-09-20 10:05:27 +0200

    fix index

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,15 @@ import { ex03 } from './exercises/ex03';
 import { ex04 } from './exercises/ex04';
 import { ex05 } from './exercises/ex05';
 import { ex06 } from './exercises/ex06';
+import { ex07 } from './exercises/ex07';
+import { ex08 } from './exercises/ex08';
+import { ex09 } from './exercises/ex09';
+import { ex10 } from './exercises/ex10';
+import { ex11 } from './exercises/ex11';
+import { ex12 } from './exercises/ex12';
+import { ex13 } from './exercises/ex13';
+import { ex14 } from './exercises/ex14';
+import { ex15 } from './exercises/ex15';
 
 function main() {
   console.log('=== Matrix Project ===');
@@ -17,6 +26,16 @@ function main() {
   ex04();
   ex05();
   ex06();
+  ex07();
+  ex08();
+  ex09();
+  ex10();
+  ex11();
+  ex12();
+  ex13();
+  ex14();
+  ex15();
+  
   console.log('\n=== All exercises completed ===');
 }
 
